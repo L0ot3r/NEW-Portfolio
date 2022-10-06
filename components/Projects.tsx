66cbd6a42459b491949618b69f1512473e56008b
leoville <sebastien.leoville@gmail.com>
2022-10-06 16:11:29 +0200

    maj 0.1.3 bg-projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -11,7 +11,7 @@ const Projects = ({ projects }: any) => {
 			<div className='section mt-[150px]'>
 				<h3 className='section-title'>Projets</h3>
 			</div>
-			<div className={`section bg-projects-img bg-fixed bg-cover bg-bottom`}>
+			<div className={`section bg-img-projects bg-fixed bg-cover bg-bottom`}>
 				<div>
 					<div className='grid md:grid-cols-3 sm:grid-cols-2 gap-4 p-5 justify-center items-center'>
 						{projects.map((project: any) => (
